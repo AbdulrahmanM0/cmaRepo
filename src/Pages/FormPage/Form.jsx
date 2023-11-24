fd032ef6c2e0ee647a15245fc68003b87b45e29a
AbdulrahmanM0 <gabdulrahman.mohammed@gmail.com>
2023-11-24 23:56:21 +0200

    build form

---
+++ src/Pages/FormPage/Form.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import * as Yup from 'yup';
+import { Formik, useFormik } from 'formik';
+import axios from 'axios';
+import { Button, Input, Spinner } from 'reactstrap';
+
+export default function FormComponent() {
+  const [formData, setFormData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('https://admin.cpvarabia.com/ZATCA/show_ZATCA_all.php');
+        const data = response.data;
+        setFormData(
+          Object.keys(data)
+            .filter((key) => key !== 'error')
+            .map((key) => data[key])
+            .sort((a, b) => a.QuestionOrder - b.QuestionOrder)
+        );
+      } catch (error) {
+        console.log('ERROR:', error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  function validateYupSchema() {
+    let schema = {};
+    formData.forEach((question) => {
+      const { Input, Required, Type, min, max } = question;
+      switch (Type) {
+        case 'text':
+          schema[Input] = Required ? Yup.string().required('*Required') : Yup.string();
+          break;
+        case 'number':
+          schema[Input] = Required
+            ? Yup.number().typeError('Must be a number').min(min, `Min ${min}`).max(max, `Max ${max}`).required('*Required')
+            : Yup.number().typeError('Must be a number').min(min, `Min ${min}`).max(max, `Max ${max}`);
+          break;
+        default:
+          break;
+      }
+    });
+    return schema;
+  }
+
+  const initialValues = {};
+  formData.forEach((question) => {
+    initialValues[question.Input] = '';
+  });
+
+  const {
+    handleChange,
+    values,
+    handleSubmit,
+    errors,
+    touched,
+    isValid,
+    setTouched
+  } = useFormik({
+    initialValues,
+    validationSchema: Yup.object().shape(validateYupSchema()),
+    onSubmit: (values, { setSubmitting }) => {
+      console.log(values);
+      setSubmitting(false);
+
+      axios.post('https://admin.cpvarabia.com/ZATCA/show_ZATCA_all.php',values)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+
+    }
+  });
+
+  useLayoutEffect(() => {
+    const initialTouchedState = {};
+    Object.keys(initialValues).forEach((key) => {
+      initialTouchedState[key] = true;
+    });
+    setTouched(initialTouchedState);
+  }, [isValid]);
+
+  return (
+    <div>
+      {formData.length > 0 ? (
+        <form onSubmit={handleSubmit}>
+          {formData.map((question) => {
+            const questionName = question.Input;
+            return (
+              <div className='form-group mb-4' key={question.QuestionOrder}>
+                <label htmlFor={questionName} className='d-flex justify-content-between mb-2'>
+                  <span>{question.QuestionOrder + "- "}{question.Required ?<>{question.QTitleEN + ' - ' + question.QTitleAR }<span className="text-danger"> *</span> </>: question.QTitleEN + ' - ' + question.QTitleAR}</span>
+                  {touched[questionName] && errors[questionName] && (
+                    <span className='form-text text-danger font-size-11'>{errors[questionName]}</span>
+                  )}
+                </label>
+                <Input
+                  className='custom-number-input Q-input font-size-12'
+                  type={question.Type}
+                  placeholder={question.Type === "number" ? `Number must be between ${question.min} ~ ${question.max}` : question.placeholder}
+                  name={questionName}
+                  onChange={handleChange}
+                  value={values[questionName] || ''}
+                />
+              </div>
+            );
+          })}
+          {!isValid &&
+          <div className='text-danger font-size-12'>
+            {"You must complete the required questions ."}
+          </div>
+          }
+          <div className='text-end m-4 me-5'>
+            <Button type='submit' disabled={!isValid}>
+              Submit
+            </Button>
+          </div>
+        </form>
+      ): <div className='text-center'><Spinner /></div>}
+    </div>
+  );
+}
---
+++ src/Pages/FormPage/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import FormComponent from './Form'
+import { Container } from 'reactstrap'
+
+export default function index() {
+  return (
+    <div className='form-page'>
+        <Container>
+            <div className='col-lg-8 col-md-10 col-sm-12 m-auto'>
+                <div className='form-container shadow-lg p-4 bg-body rounded'>
+                    <FormComponent />
+                </div>
+            </div>
+        </Container>
+    </div>
+  )
+}
