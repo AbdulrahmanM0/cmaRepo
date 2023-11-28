bee7482b53682176830c12c54e7cecc458c89efc
AbdulrahmanM0 <gabdulrahman.mohammed@gmail.com>
2023-11-28 16:29:48 +0200

    center footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='p-3 bg-dark text-white font-size-12'>
-      <div className='d-flex justify-content-between'><span className='fw-bold'>CPV ARABIA </span><span>©All rights reserved .</span></div>
+      <div className='text-center'><span className='fw-bold'>CPV ARABIA </span></div>
     </footer>
   )
 }
